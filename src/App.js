458bad9befe86a07ab9fb71a709e3fed6f7d4ddd
Ashish <kumar.29ashish.95.@gmail.com>
2024-02-08 15:45:51 +0530

    css setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import About from './components/about/About';
+import Header from './components/header/Header';
 import Navbar from './components/nav/Navbar';
+import Experience from './components/experience/Experience';
+import Testimonials from './components/testimonials/Testimonials';
+import Services from './components/services/Services';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer'
 function App() {
   return (
-    <div className="App" >
-    <Navbar/>
-     
-    </div>
+    <>
+      <Header/>
+      <Navbar/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+      
+      
+
+      
+
+
+    </>
   );
 }
 
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './about.css'
 const About = () => {
   return (
     <div>
-      
+      <h1>Stan</h1>
     </div>
   )
 }
