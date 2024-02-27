c5c66fad4c1d4a614ce0ea7f6c0ca12446b9a430
Ashish <kumar.29ashish.95.@gmail.com>
2024-02-28 02:52:01 +0530

    start

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/gippy.jpg'
+import ME from '../../assets/coding.png'
 import HeaderSocials from './HeaderSocials'
 
 
@@ -17,7 +17,7 @@ const Header = () => {
           <HeaderSocials/>
 
           <div className='me'>
-            <img src={ME} alt='my-image'/>
+            <img className='me_img' src={ME} alt='my-image'/>
           </div>
 
           <a href='#contact' className='scroll__down'>Scroll Down</a>
