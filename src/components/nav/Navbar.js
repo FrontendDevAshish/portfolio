efb62aa2224e8bacb51badfd6f2fd558eec5b070
Ashish <kumar.29ashish.95.@gmail.com>
2024-03-01 19:10:47 +0530

    nav setup

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './navbar.css'
+import { MdOutlineHome } from "react-icons/md";
+
+
 const Navbar = () => {
   return (
-    <div >
+    <nav>
 
-      
-    </div>
+      <a href='#'><MdOutlineHome/></a>
+      <a href='#about'><MdOutlineHome/></a>
+      <a href='#experience'><MdOutlineHome/></a>
+      <a href='#services'><MdOutlineHome/></a>
+      <a href='#contact'><MdOutlineHome/></a>
+      {/* <p className='new feats'>If you are inthe midst of the</p> */}
+
+    </nav>
+
+  
    )
 }
 
