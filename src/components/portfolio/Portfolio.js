81ba503d929eea1b65152374ee650ead4a6fdcba
Ashish <kumar.29ashish.95.@gmail.com>
2024-04-19 01:10:43 +0530

    transitions added

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -5,9 +5,46 @@ import IMG2 from '../../assets/IMG2.jpg'
 import IMG3 from '../../assets/IMG3.jpg'
 import IMG4 from '../../assets/IMG4.jpg'
 import IMG5 from '../../assets/IMG5.jpg'
-import IMG6 from '../../assets/IMG6.jpg'
+// import IMG6 from '../../assets/IMG6.jpg'
 
-
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Crypto app',
+    github: 'https://github.com',
+    demo : 'https://dribbble.com/Alien_pixels'
+  },
+  {
+    id: 2,
+    image: IMG1,
+    title: 'Crypto app',
+    github: 'https://github.com',
+    demo : 'https://dribbble.com/Alien_pixels'
+  },
+  {
+    id: 3,
+    image: IMG1,
+    title: 'Crypto app',
+    github: 'https://github.com',
+    demo : 'https://dribbble.com/Alien_pixels'
+  },
+  {
+    id: 4,
+    image: IMG1,
+    title: 'Crypto app',
+    github: 'https://github.com',
+    demo : 'https://dribbble.com/Alien_pixels'
+  },
+  {
+    id: 5,
+    image: IMG1,
+    title: 'Crypto app',
+    github: 'https://github.com',
+    demo : 'https://dribbble.com/Alien_pixels'
+  },
+  
+]
 
 const Portfolio = () => {
   return (
@@ -17,31 +54,21 @@ const Portfolio = () => {
         <h2>Portfolio</h2>
 
         <div className='container portfolio__container'>
-          <article className='portfolio__item'>
+          {
+            data.map(({id, image, title, demo, github}) =>{
+              return (
+            <article key={id} className='portfolio__item'>
             <div className='portfolio__item-image'>
-              <img src={IMG1} alt=''/>
+              <img src={image} alt={title}/>
             </div>
-            <h3>This is a portfolio item title</h3>
-            <a href='https://github.com' className='btn' target='_blank'>Github</a>
-            <a href='https://dribbble.com/Alien_pixels' className='btn btn-primary' target='_blank'>Live Demo</a>
-          </article>
-          <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-            <img src={IMG2} alt=''/>
+            <h3>{title}</h3>
+            <div className='portfolio__item-cta'>
+            <a href={github} className='btn' target='_blank'>Github</a>
+            <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
             </div>
-            <div className='portfolio__item-image'>
-            <img src={IMG2} alt=''/>
-            </div>
-            <div className='portfolio__item-image'>
-            <img src={IMG2} alt=''/>
-            </div>
-            <div className='portfolio__item-image'>
-            <img src={IMG2} alt=''/>
-            </div>
-            <div className='portfolio__item-image'>
-            <img src={IMG2} alt=''/>
-            </div>
-          </article>
+          </article>)
+            })
+          }
         </div>
       </section>
     </div>
