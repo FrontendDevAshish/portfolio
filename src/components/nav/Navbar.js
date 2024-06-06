01d43fb6c7c9b4149a78b98ec8abef53ea9d1c13
Ashish <kumar.29ashish.95.@gmail.com>
2024-06-06 20:06:19 +0530

    section completed

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import './navbar.css'
-import { MdOutlineBook, MdOutlineDesignServices, MdOutlineHome, MdOutlineMessage, MdOutlineVerifiedUser } from "react-icons/md";
-import { useState } from 'react';
-
+import { MdOutlineBook, MdOutlineDesignServices, MdOutlineHome, MdOutlineMessage, MdOutlineVerifiedUser } from "react-icons/md"
+import { useState } from 'react'
 
 const Navbar = () => {
   const [activeNav,setActiveNav] = useState('#')
   return (
     <nav>
-
       <a href='#' className={activeNav === '0'?'active':''}><MdOutlineHome/></a>
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about'?'active':''}><MdOutlineVerifiedUser/></a>
       <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience'?'active':''}><MdOutlineBook/></a>
       <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav === '#services'?'active':''}><MdOutlineDesignServices/></a>
       <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact'?'active':''}><MdOutlineMessage/></a>
       
-
     </nav>
 
-  
+
    )
 }
 
