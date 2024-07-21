138d52592cd26e4aaa42b786298638ea709c740f
Ashish <kumar.29ashish.95.@gmail.com>
2024-07-21 20:50:44 +0530

    testimonials

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -1,10 +1,65 @@
 import React from 'react'
+import './testimonials.css'
+import Gippy from '../../assets/gippy.jpg'
 
 const Testimonials = () => {
+  const data =  [
+    {
+      avatar:Gippy,
+      name:'Lilly aldrin',
+      review: 'lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum '
+    },
+    {
+      avatar:Gippy,
+      name:'Lilly aldrin',
+      review: 'lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum '
+    },
+    {
+      avatar:Gippy,
+      name:'Lilly aldrin',
+      review: 'lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum '
+    },
+    {
+      avatar:Gippy,
+      name:'Lilly aldrin',
+      review: 'lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum '
+    },
+    {
+      avatar:Gippy,
+      name:'Lilly aldrin',
+      review: 'lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum '
+    },
+    {
+      avatar:Gippy,
+      name:'Lilly aldrin',
+      review: 'lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum lorem dolor ipsum '
+    }
+  ]
+
   return (
-    <div>
-      
-    </div>
+    <section id ='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className='testimonials__container'>
+        {
+          data.map(({avatar,name,review},index) =>{
+            return(
+              <article key={index} className='testimonial'>
+              <div className='client__avatar'>
+                <img src={avatar} alt='avatar one'/>
+              </div>
+              <h5 className='client__name'>{name}</h5>
+              <small className='client__review'>
+              {review}
+              </small>
+
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
