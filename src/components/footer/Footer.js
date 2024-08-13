957171ced5d338ca5fdc7d40c542f714b975004b
Ashish <kumar.29ashish.95.@gmail.com>
2024-08-13 16:14:17 +0530

    setup completed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { FaFacebook } from 'react-icons/fa'
+import './footer.css'
 
 const Footer = () => {
   return (
-    <div>
-    <h1>This is fooking footer</h1>
-    </div>
+    <footer>
+      <a href='#' className='footer__logo'>Footer</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+
+      <a href='https://facebook.com'><FaFacebook/></a>
+      <a href='https://facebook.com'><FaFacebook/></a>
+      <a href='https://facebook.com'><FaFacebook/></a> 
+      </div>
+      <div className='footer__copyright'>
+        <samll> Frontend_Dev : " Aashish Kumar " </samll>
+      </div>
+    </footer>
   )
 }
 
